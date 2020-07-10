b852e6166574bd526c831c2427d42a16d63e23b6
parthasarathi <me.parthu@gmail.com>
2020-07-10 11:26:43 +0530

    Adding Reusable Navigation items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Layout from './components/layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
