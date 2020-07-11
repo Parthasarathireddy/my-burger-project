d783bb1b8cbb6cef6efe93d5e2fb3dd831251762
parthasarathi <me.parthu@gmail.com>
2020-07-11 11:33:13 +0530

    Changing folder structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Layout from './components/layout/Layout';
+import Layout from './hoc/layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 function App() {
   return (
