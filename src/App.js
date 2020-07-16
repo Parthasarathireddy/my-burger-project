c1ec2dee7e2842f90ebef0f7031a9e00d9bdf44e
parthasarathi <me.parthu@gmail.com>
2020-07-16 09:39:38 +0530

    removed comment code in App.js file

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import './App.css';
 import Layout from './hoc/layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
-  //Checking Purposs we added this 
-  // state = {
-  //   show : true
-  // }
-  // componentDidMount(){
-  //   setTimeout(()=> {
-  //     this.setState({show: false});
-  //   },5000);
-  // }
   render() {
     return (
       <div>
         <Layout>
-         {/* {this.state.show ? <BurgerBuilder /> : null } */}
           <BurgerBuilder /> 
         </Layout>
       </div>
