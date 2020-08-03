e072eca9d50836acd503976b75b47a42438f6427
parthasarathi <me.parthu@gmail.com>
2020-08-03 11:16:12 +0530

    custom dynamic input component and handling form submit

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,7 +83,6 @@ class BurgerBuilder extends Component{
         }
         queryParams.push("price=" + this.state.totalPrice);
         const queryString = queryParams.join('&');
-
         this.props.history.push({
             pathname: '/checkout',
             search: '?' + queryString
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,35 +3,85 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../Axios-orders';
-class ContactData extends Component{
+import Input from '../../../components/UI/Input/Input';
+class ContactData extends Component {
     state = {
-        name : '',
-        email : '',
-        address : {
-            street : '',
-            postalCode : ''
+        orderForm: {
+            name: {
+                elementType : 'input',
+                elemenentConfig : {
+                    type : 'text',
+                    placeholder : 'Your Name'
+                },
+                value : ''
+            },
+            Street: {
+                elementType : 'input',
+                elemenentConfig : {
+                    type : 'text',
+                    placeholder : 'Your Street'
+                },
+                value : ''
+            },
+            state: {
+                elementType : 'input',
+                elemenentConfig : {
+                    type : 'text',
+                    placeholder : 'Your State'
+                },
+                value : ''
+            },
+            zipcode: {
+                elementType : 'input',
+                elemenentConfig : {
+                    type : 'text',
+                    placeholder : 'Your Zipcode'
+                },
+                value : ''
+            },
+            country: {
+                elementType : 'input',
+                elemenentConfig : {
+                    type : 'text',
+                    placeholder : 'Your Country'
+                },
+                value : ''
+            },
+            email: {
+                elementType : 'input',
+                elemenentConfig : {
+                    type : 'email',
+                    placeholder : 'Your E-Mail'
+                },
+                value : ''
+            },
+            deliveryMethod: {
+                elementType : 'select',
+                elemenentConfig : {
+                    options : [
+                        {value : 'fastest', displayValue : 'Fastest'},
+                        {value : 'cheapest', displayValue : 'Cheapest'}
+                            ]
+                },
+                value : ''
+            },
         },
-        loading : false,
-        totalPrice : 4
+        loading: false
     }
+
     orderHandler = (event) => {
         event.preventDefault();
         console.log(this.props);
         this.setState({ loading: true });
         // alert('You Continue!');
+        const formData = {};
+        for (let formElementidentifier in this.state.orderForm){
+            formData[formElementidentifier] = this.state.orderForm[formElementidentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            Customer: {
-                name: 'Partha',
-                address: {
-                    Street: 'Bachupalli',
-                    state: 'Telangana',
-                    zipcode: '500090'
-                },
-                email: 'me@me.com'
-            },
-            deliveryMethod: 'fastest'
+            orderData : formData
         }
         axios.post('/orders.json', order).then(response => {
             console.log(response);
@@ -42,22 +92,42 @@ class ContactData extends Component{
             console.log(error);
         });
     }
-    render(){
-        let form = ( <form>
-            <input className = {classes.Input} type ='text' name = "name" placeholder = "Enter your Name" />
-            <input className = {classes.Input} type ='email' name = "email" placeholder = "Enter your Email" />
-            <input className = {classes.Input} type ='text' name = "street" placeholder = "Enter your Street" />
-            <input className = {classes.Input} type ='text' name = "postal" placeholder = "Enter Postal Code" />
-            <Button btnType = "Success" clicked = {this.orderHandler}>ORDER</Button>
+    inputChangedHandler = (event, inputIdentifier) =>{
+        //console.log(event.target.value);
+        //spread operator not clone deeply
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm : updatedOrderForm});
+
+    }
+    render() {
+        const formElementArray = [];
+        for (let key in this.state.orderForm){
+            formElementArray.push({
+                id : key,
+                config : this.state.orderForm[key]
+            })
+        }
+        let form = (<form onSubmit = {this.orderHandler}>
+            {formElementArray.map(formElement =>(
+                <Input key = {formElement.id} elementType = {formElement.config.elementType} elemenentConfig = {formElement.config.elemenentConfig} value = {formElement.config.value} changed = {(event) => {this.inputChangedHandler(event, formElement.id)}}/>
+            ))}
+            <Button btnType="Success">ORDER</Button>
         </form>);
-        if(this.state.loading){
-            form  = <Spinner />
+        if (this.state.loading) {
+            form = <Spinner />
         }
 
         return (
-            <div className = {classes.ContactData}>
+            <div className={classes.ContactData}>
                 <h4>Enter your contact data</h4>
-               {form}
+                {form}
             </div>
         );
     }
